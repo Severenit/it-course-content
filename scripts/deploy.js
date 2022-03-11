b94240b9ed11399ba7c9bd9d42c894999a044198
Zar Zakharov <severenit@gmail.com>
2022-03-11 12:08:50 +0300

    Build

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,34 @@
 const path = require('path');
 const fs = require('fs');
 
-console.log(path.resolve(__dirname, '/course'));
+const pathCourse = path.resolve(__dirname, '..', 'course');
+console.log(pathCourse);
 
-fs.readdir(path.resolve(__dirname, '/course'), (err, files) => {
-  console.log(err);
+const readDir = async (pathDir, cb) => {
+  fs.readdir(pathDir, (err, files) => {
+    console.log(err);
+    const course = {};
+    if (!err) {
+      files.forEach(file => {
+        const pathFile = path.resolve(pathDir, file);
 
-  files.forEach(file => {
-    console.log(file);
+        if (fs.lstatSync(pathFile).isDirectory()) {
+          course[file] = pathFile;
+        } else {
+          // console.log(`####: file ${pathDir} `, file);
+        }
+      });
+      cb && cb(course);
+    }
   });
-})
+}
+
+readDir(pathCourse, (course) => {
+  Object.entries(course).forEach(item => {
+    fs.readFile(path.resolve(item[1], 'main.json'), 'utf8', (error, content) => {
+      console.log('####: c', item[0]);
+      console.log('####: c', content);
+    })
+  })
+});
+

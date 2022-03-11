219d9e7d1da18351420793dc16236336198bc06e
Zar Zakharov <severenit@gmail.com>
2022-03-11 11:10:47 +0300

    Some Commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,12 @@
 const path = require('path');
+const fs = require('fs');
 
 console.log(path.resolve(__dirname, '/course'));
+
+fs.readdir(path.resolve(__dirname, '/course'), (err, files) => {
+  console.log(err);
+
+  files.forEach(file => {
+    console.log(file);
+  });
+})
